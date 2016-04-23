04728ae1619fdf645b92ac0a8f1b153f2a13c22e
pauljohanneskraft <pauljohanneskraft@Pauls-MacBook.local>
2016-04-23 21:49:07 +0200

    added Error-file, where all the (future) ErrorTypes can be found

---
+++ Matrices/Error.swift
@@ -0,0 +1,13 @@
+//
+//  Error.swift
+//  Matrices
+//
+//  Created by Paul Kraft on 23.04.16.
+//  Copyright © 2016 pauljohanneskraft. All rights reserved.
+//
+
+// import Cocoa
+
+enum MatrixError: ErrorType {
+    case NotMultipliable, NotAddable, Unsolvable
+}
