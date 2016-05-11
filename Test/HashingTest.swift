87d3958d52d1dbcd24d4ed5536cff94a988eb259
pauljohanneskraft <pauljohanneskraft@Pauls-MacBook.local>
2016-05-12 01:12:51 +0200

    +HashingTest

---
+++ Test/HashingTest.swift
@@ -0,0 +1,23 @@
+//
+//  HashingTest.swift
+//  Math
+//
+//  Created by Paul Kraft on 12.05.16.
+//  Copyright © 2016 pauljohanneskraft. All rights reserved.
+//
+
+import XCTest
+
+class HashingTest: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+}
