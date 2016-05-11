e7112b7c2b0097760daaaa374b9ba0902ead2ee8
pauljohanneskraft <pauljohanneskraft@Pauls-MacBook.local>
2016-05-12 00:39:30 +0200

    significant improvements

--- Math/Hashing.swift
+++ Math/Hashing.swift
@@ -8,40 +8,105 @@
 
 import Foundation
 
-struct HashList <T: NumericType, U> {
+struct HashTable <T: NumericType, U> : CustomStringConvertible {
     
-    private(set) var list = [[(key: T, value: U)]]()
-    private(set) var m : UInt = 10
+    private(set) var table = [[(key: T, value: U)]]()
+    private(set) var hash: T -> Int
     
-    init(_ count : UInt) {
-        m = count
-        list = [[(key: T, value: U)]]()
-        for _ in 0..<count {
-            list.append([])
-        }
+    init(_ count : Int) { // convenience init
+        self.init(_: count, hash: { return Int($0 % T(count)) })
     }
     
-    mutating func insert(new: (key: T, value: U)) throws {
+    init(_ count : Int, hash: T -> Int) {
+        table = [[(key: T, value: U)]](count: count, repeatedValue: [])
+        self.hash = hash
+    }
+    
+    mutating func insert(key key: T, value: U) throws {
+        try insert(element: (key, value))
+    }
+    
+    mutating func insert(element new: (key: T, value: U)) throws {
         let h = hash(new.key)
-        for v in list[h] {
-            if new.key == v.key {
-                throw HashListError.AlreadyInList
+        if !table.range.contains(h) {
+            throw HashTableError.BadHashFunction
+        }
+        for v in table[h].range {
+            if new.key == table[h][v].key {
+                throw HashTableError.InList
             }
         }
-        list[h].append(new)
+        table[h].append(new)
     }
     
-    func hash(value : T) -> Int {
-        return Int(value % T(m))
+    mutating func find(key key: T) throws -> (key: T, value: U) {
+        let h = hash(key)
+        if !table.range.contains(h) {
+            throw HashTableError.BadHashFunction
+        }
+        for v in table[h].range {
+            if key == table[h][v].key {
+                return table[h][v]
+            }
+        }
+        throw HashTableError.NotInList
+    }
+    
+    mutating func remove(key old: T) throws -> (key: T, value: U) {
+        let h = hash(old)
+        if !table.range.contains(h) {
+            throw HashTableError.BadHashFunction
+        }
+        for v in table[h].range {
+            if old == table[h][v].key {
+                return table[h].removeAtIndex(v)
+            }
+        }
+        throw HashTableError.NotInList
+    }
+    
+    private func goodHashFunction() -> Bool {
+        var min = 0
+        var max = 0
+        var total = 0
+        for i in 0..<Int(table.count) {
+            let length = table[i].count
+            if table[min].count > length { min = i }
+            if table[max].count < length { max = i }
+            total += length
+        }
+        let avg = total / Int(table.count)
+        return max - min < avg
+    }
+    
+    mutating func changeHash(hash newHash: T -> Int) throws {
+        let oldHash = self.hash
+        let oldTable = self.table
+        do {
+            self.hash = newHash
+            self.table = [[(key: T, value: U)]](count: oldTable.count, repeatedValue: [])
+            for bucket in oldTable {
+                for element in bucket {
+                    try insert(element: element)
+                }
+            }
+        } catch _ {
+            self.hash = oldHash
+            self.table = oldTable
+            throw HashTableError.BadHashFunction
+        }
+    }
+    
+    var description: String {
+        return String(table)
     }
     
 }
 
 
-
-
-
-
+struct Test {
+    var String = "hallo"
+}
 
 
 
