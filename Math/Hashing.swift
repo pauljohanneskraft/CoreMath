b4c9778631548742a4404870d2a78f92f14ae514
pauljohanneskraft <pauljohanneskraft@Pauls-MacBook.local>
2016-05-11 22:57:50 +0200

    hashtable having (key: T, value: U)-tuples

--- Math/Hashing.swift
+++ Math/Hashing.swift
@@ -8,33 +8,32 @@
 
 import Foundation
 
-struct HashList <T: NumericType> {
+struct HashList <T: NumericType, U> {
     
-    private(set) var list = [[T]]()
+    private(set) var list = [[(key: T, value: U)]]()
     private(set) var m : UInt = 10
     
     init(_ count : UInt) {
         m = count
-        list = [[T]]()
+        list = [[(key: T, value: U)]]()
         for _ in 0..<count {
             list.append([])
         }
     }
     
-    mutating func insert(value : T) throws {
-        let h = hash(value)
+    mutating func insert(new: (key: T, value: U)) throws {
+        let h = hash(new.key)
         for v in list[h] {
-            if value == v {
+            if new.key == v.key {
                 throw HashListError.AlreadyInList
             }
         }
-        list[h].append(value)
+        list[h].append(new)
     }
     
     func hash(value : T) -> Int {
         return Int(value % T(m))
     }
-    
     
 }
 
