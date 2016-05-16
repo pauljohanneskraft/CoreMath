7fe141a535f5645a21ebe6924c4347ba097f3a70
pauljohanneskraft <pauljohanneskraft@Pauls-MacBook.local>
2016-05-16 22:19:43 +0200

    protocols for basic arithmetic

---
+++ Math/Protocols.swift
@@ -0,0 +1,31 @@
+//
+//  Protocols.swift
+//  Math
+//
+//  Created by Paul Kraft on 15.05.16.
+//  Copyright © 2016 pauljohanneskraft. All rights reserved.
+//
+
+import Cocoa
+
+protocol ElementaryCalculation : Addable, Subtractable, Multipliable, Divisible {}
+
+protocol Multipliable {
+    func * (left: Self, right: Self) -> Self
+    func *=(inout left: Self, right: Self)
+}
+
+protocol Addable {
+    func + (left: Self, right: Self) -> Self
+    func += (inout left: Self, right: Self)
+}
+
+protocol Subtractable {
+    func - (left: Self, right: Self) -> Self
+    func -= (inout left: Self, right: Self)
+}
+
+protocol Divisible {
+    func / (left: Self, right: Self) -> Self
+    func /= (inout left: Self, right: Self)
+}
