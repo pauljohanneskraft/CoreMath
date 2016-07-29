f5f5cec225e3f3f735fab86451d664f42bfefcb5
Paul <pauljohanneskraft@users.noreply.github.com>
2016-07-30 00:59:50 +0200

    + Term, often used Terms in TrigonometricFunctions.swift
    —> needs renaming, because also logarithmic examples in file

--- Math/Function.swift
+++ Math/Function.swift
@@ -14,6 +14,49 @@ protocol Function {
     func call(x: Operand) -> Result
 }
 
+struct Term < Number : BasicArithmetic > : Function, CustomStringConvertible {
+    
+    var term : (Number) -> Number
+    var sign : Bool = false
+    var description: String
+    
+    init(description: String, sign : Bool = false, _ term: (Number) -> Number) {
+        self.term = term
+        self.sign = sign
+        self.description = description
+    }
+    
+    func call(x: Number) -> Number {
+        return sign ? -term(x) : term(x)
+    }
+}
+
+func + <N : BasicArithmetic>(lhs: Term<N>, rhs: Term<N>) -> Equation<N> {
+    return Equation(terms: [lhs, rhs])
+}
+
+struct Equation < Number : BasicArithmetic > : Function, CustomStringConvertible {
+    typealias T = Term < Number >
+    
+    var terms : [ T ]
+    
+    func call(x: Number) -> Number {
+        var value : Number = 0
+        for t in terms { value += t.call(x: x) }
+        return value
+    }
+    
+    var description : String {
+        if terms.isEmpty { return "0" }
+        var result = "\(terms.first!)"
+        for t in terms.dropFirst() {
+            result += (t.sign ? " - " : " + ") + "\(t)"
+        }
+        return result
+    }
+    
+}
+
 struct FunctionWrapper < F : Function > : CustomStringConvertible {
     var function : F
     var name : String
