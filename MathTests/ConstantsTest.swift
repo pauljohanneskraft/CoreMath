685dd34b01bbcaf26c92423760f7269430b6986b
Paul <pauljohanneskraft@users.noreply.github.com>
2016-08-25 19:59:20 +0200

    printing many constants now, still needs to be checked by someone...

--- MathTests/ConstantsTest.swift
+++ MathTests/ConstantsTest.swift
@@ -21,9 +21,33 @@ class ConstantsTest: XCTestCase {
     }
 	
 	func testGoldenRatio() {
-		print(Constants.Math.goldenRatio)
-		print(Constants.Physics.e_0)
-		print(Constants.Physics.h_)
+		print("pi:         ", Constants.pi)
+		XCTAssert(Constants.pi == Double.pi)
+		print("-----------------------------------------------------")
+		print("tau:        ", Constants.tau)
+		XCTAssert(Constants.tau == Double.pi * 2)
+		print("-----------------------------------------------------")
+		print("e:          ", Constants.Math.e)
+		print("-----------------------------------------------------")
+		print("gamma:      ", Constants.Math.gamma)
+		print("-----------------------------------------------------")
+		print("i:          ", Constants.Math.i)
+		print("-----------------------------------------------------")
+		print("goldenRatio:", Constants.Math.goldenRatio)
+		print("-----------------------------------------------------")
+		print("c:          ", Constants.Physics.c)
+		print("-----------------------------------------------------")
+		print("e:          ", Constants.Physics.e)
+		print("-----------------------------------------------------")
+		print("G:          ", Constants.Physics.G)
+		print("-----------------------------------------------------")
+		print("h:          ", Constants.Physics.h)
+		print("-----------------------------------------------------")
+		print("e_0:        ", Constants.Physics.e_0)
+		print("-----------------------------------------------------")
+		print("h_:         ", Constants.Physics.h_)
+		print("-----------------------------------------------------")
+		print("y_0:        ", Constants.Physics.y_0)
 	}
     
 }
